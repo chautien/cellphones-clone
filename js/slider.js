970ab05740da4bfcd3cda080b8d1914db818f115
Chau Tien <61677225+chautien@users.noreply.github.com>
2020-11-23 09:34:01 +0700

    update

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,17 @@
 $(document).ready(function () {
   $(".slider-for").slick({
+    slide: ".slider-for-item",
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false,
+    arrows: true,
     fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
     asNavFor: ".slider-nav",
+    accessibility: true,
   });
   $(".slider-nav").slick({
+    slide: ".slider-nav-item",
     slidesToShow: 3,
     slidesToScroll: 1,
     asNavFor: ".slider-for",
